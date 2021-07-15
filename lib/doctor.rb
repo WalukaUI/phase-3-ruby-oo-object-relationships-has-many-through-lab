3e47703e193494400b038374f117bb0c038cc23f
WalukaUI <cwaluka@yahoo.com>
2021-07-14 22:08:18 -0700

    completed

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,21 @@
+class Doctor
+    attr_accessor :name
+    @@all=[]
+    def initialize(name)
+      @name=name
+      @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def new_appointment(date,patient)
+      Appointment.new(date,patient,self)
+    end
+    def appointments
+        Appointment.all.select {|date| date.doctor == self}
+    end
+    def patients
+        appointments.map {|date| date.patient}
+    end
 
+end
